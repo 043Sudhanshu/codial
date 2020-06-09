9d9c1639d65cfb5d440aaff015ddbc9c88860857
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-06-10 03:37:27 +0530

    added route inside the route and controllers corresponding to them

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express=require("express");
 
 const routes=express.Router();
 
-const homecontroller=require('../contorllers/home_controller');
+const homecontroller=require('../controllers/home_controller');
 
 routes.get('/',homecontroller.home);
-
+routes.use('/user',require('./user'));
 module.exports=routes;
 
