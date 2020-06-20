d2f4a23cc6d3e88e14cb09b9cb1d22faa41cb35f
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-06-21 01:00:47 +0530

    created the signup controller which verifies whether the user exists or not,if it exist then redirect to same page and if dosent exist make the user in db and render the sign in page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,15 +1,35 @@
-
+const User=require('../model/user');
 module.exports.profile=function(req,res){
     res.render('profile',{title:"profile"});
 }
 module.exports.create=function(req,res){
-   
-    
-    
-}
-module.exports.signup=function(req,res){
+   if(req.body.password!=req.body.confirm_password){
+      res.redirect('back');
+   }
+   else{
+    User.findOne({email:req.body.email},function(err,data){
+        if(err){console.log("user finding error:"+err); return;}
+        if(!data){
+         User.create(req.body,function(err,user){
+             if(err){console.log("error in creating user"); return;}
+             res.render('user_sign_in',{title:"signin"});
+         });
+        }
+        else{
+        console.log("this username exists");
+        return;
+        }
+        });
+    }
 
+    }   
+
+
+
+module.exports.signup=function(req,res){
+    
     res.render('user_sign_up',{title:"signup"});
+
 }
 
 module.exports.createsession=function(req,res){
